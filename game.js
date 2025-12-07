fd203f890e1587abe2b044217949d4599c1a2262
WolfouLOLP <dejuliistony9@gmail.com>
2025-12-07 20:13:51 +0100

    Update game.js

--- game.js
+++ game.js
@@ -762,7 +762,7 @@ function animateTokens(count){
 const musics = [
   document.getElementById('music1'),
   document.getElementById('music2')
-  document.getElementById('music3')
+  
 ];
 let currentMusic = 0;
 
@@ -776,5 +776,6 @@ document.getElementById('change-music').onclick = () => {
   currentMusic = (currentMusic + 1) % musics.length;
   musics[currentMusic].play().catch(err => console.log(err));
 };
+
 
 
